8aeadf22ea3ca6e128a8deb0699f3b5843f170f9
kjdevelop <115779924+kjdevelop@users.noreply.github.com>
2023-08-20 16:09:30 +0800

    Update main.py
    fix syntax

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from src.enums import Instructions, States, Actions
-from LineAPI import LineAPI, fetch_instructions
+from src.LineAPI import LineAPI, fetch_instructions
 
 
 async def process_instruction(instruction: Instructions = Instructions.INVALID, state: States = States.INVALID, user_interface = None):
@@ -33,24 +33,23 @@ async def process_instruction(instruction: Instructions = Instructions.INVALID, 
             user_interface.reject_instruction(message = 'Currently turned off.')
     
     elif state == States.ONLINE:
-        match instruction:
-            case Instructions.START: 
+            if instruction == Instructions.START: 
                 user_interface.reject_instruction(message = 'Already started.')
-            case Instructions.END: 
+            elif instruction == Instructions.END: 
                 print('Ending...')
                 await asyncio.sleep(2)
                 status = Actions.HALT
-            case Instructions.MOVE:
+            elif instruction == Instructions.MOVE:
                 print('Start moving...')
                 await asyncio.sleep(2)
                 print('End moving')
-            case Instructions.RETURN:
+            elif instruction == Instructions.RETURN:
                 print('Start moving...')
                 await asyncio.sleep(2)
                 print('End moving')
-            case Instructions.INVALID:
+            elif instruction == Instructions.INVALID:
                 user_interface.reject_instruction(message = 'Invalid instruction')
-            case _:
+            else:
                 user_interface.reject_instruction(message = 'Invalid instruction')
     
     return status
@@ -70,10 +69,10 @@ async def instruction_consumer(queue: asyncio.Queue, user_interface) -> None:
 
     Raises: None
     """
-    
+    print("IC hello")
     while True:
+        print(len(queue._queue))
         instruction, state = await queue.get()
-
         if instruction == Instructions.LOG:
             print(f'Queue contents: {list(queue._queue)}')
             continue
@@ -91,16 +90,16 @@ async def main():
     user_interface = LineAPI()
 
     # Instruction Queue
-    instruction_queue_task = asyncio.create_task(instruction_consumer(instruction_queue))
+    instruction_queue_task = asyncio.create_task(instruction_consumer(instruction_queue, user_interface))
     # Human Detection
-    detection_task = asyncio.create_task()
+    # detection_task = asyncio.create_task()
    
     state = States.IDLE
 
     while True:
-        instruction: Instructions = await fetch_instructions()
+        instruction: Instructions = await fetch_instructions(user_interface)
         await instruction_queue.put((instruction, state))
-        user_interface.send_message(f'Added instruction: {instruction}, {len(instruction_queue._queue)} in line.')
+        user_interface.log_reply(f'Added instruction: {instruction}, {len(instruction_queue._queue)} in line.')
 
         if state == States.IDLE and instruction == Instructions.START:
             state = States.ONLINE
