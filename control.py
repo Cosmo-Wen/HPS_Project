a8bff9202bf4b2256648da3814fe15c37db04014
Cosmo-Wen <forhchs@gmail.com>
2023-08-20 17:59:42 +0800

    feat: modularize lid control

--- control.py
+++ control.py
@@ -5,54 +5,73 @@ PIN_TRIGGER = 7
 PIN_ECHO = 11
 PIN_SERVO = 12
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-GPIO.setup(PIN_ECHO, GPIO.IN)
-GPIO.setup(PIN_SERVO, GPIO.OUT)
 
-pwm = GPIO.PWM(PIN_SERVO, 50)
-pwm.start(4)
 
 class Lid:
     def __init__(self):
-        self.run = False
+        self._run = False
+        self._flag = False
 
-    def sense(self):
-        flag = False
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+        GPIO.setup(PIN_ECHO, GPIO.IN)
+        GPIO.setup(PIN_SERVO, GPIO.OUT)
 
-        while self.run:
+        self._pwm = GPIO.PWM(PIN_SERVO, 50)
+        self._pwm.start(4)
+    
+    def turn_on(self):
+        self._run = True
+    
+    def turn_off(self):
+        self._run = False
+    
+    def current_status(self):
+        return self._run
+
+    def change_duty_cycle(self, amount):
+        self._pwm.ChangeDutyCycle(amount)
+    
+    def shutdown(self):
+        self._pwm.stop()
+        GPIO.cleanup()
+
+    async def sense(self):
+        while self.current_status():
             #Trigger
             GPIO.output(PIN_TRIGGER, GPIO.HIGH)
             sleep(0.00001)
             GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-            while GPIO.input(PIN_ECHO)==0:
+            while GPIO.input(PIN_ECHO) == 0:
                 pulse_start_time = time()
-            while GPIO.input(PIN_ECHO)==1:
+
+            while GPIO.input(PIN_ECHO) == 1:
                 pulse_end_time = time()
 
             pulse_duration = pulse_end_time - pulse_start_time
+            
             distance = round(pulse_duration * 17150, 2)
+            
             print("Distance:",distance,"cm")
 
-            if(distance < 10):
-                if(flag==False):
-                    pwm.ChangeDutyCycle(7.5)
+            if distance < 10:
+                if not self._flag :
+                    self._pwm.ChangeDutyCycle(7.5)
                     sleep(5)
-                    pwm.ChangeDutyCycle(4)
+                    self._pwm.ChangeDutyCycle(4)
                     sleep(1)
-                flag = True
+                self._flag = True
             else:
-                flag = False
+                self._flag = False
 
             sleep(1)
-
-lid = Lid()
+    
 try:
+    lid = Lid()
     lid.sense()
     sleep(5)
-    lid.run = True
+    lid.turn_on()
     lid.sense()
 finally:
-    pwm.stop()
-    GPIO.cleanup()
+    lid.shutdown()
